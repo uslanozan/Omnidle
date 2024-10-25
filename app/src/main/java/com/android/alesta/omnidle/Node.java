26f0bf8713b0ad526daccba10b7eb6293d2ecf99
Süleyman Emre Parlak <146767767+paitblack@users.noreply.github.com>
2024-10-26 01:44:25 +0300

    algo first commit 26.10.2024

---
+++ app/src/main/java/com/android/alesta/omnidle/Node.java
@@ -0,0 +1,13 @@
+class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public String toString() {
+        return String.valueOf(data);
+    }
+}
